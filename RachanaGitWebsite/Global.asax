4cad4270e25cfc41be136e1bbfed0ad6f7fafc91
rachanajadhav <rachana.borse@gmail.com>
2014-05-08 14:22:32 +0530

    First Commit

---
+++ RachanaGitWebsite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="RachanaGitWebsite.Global" Language="C#" %>
---
+++ RachanaGitWebsite/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(RachanaGitWebsite.Startup))]
+namespace RachanaGitWebsite
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
